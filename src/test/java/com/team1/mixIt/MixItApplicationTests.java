73348d0b4e4da776e56b0e8d7e0f899b9850aef7
Gwithjoy <bangbabo131@gmail.com>
2025-04-11 17:13:16 +0900

    Initial commit

---
+++ src/test/java/com/team1/mixIt/MixItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.mixIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
