89739e9538f1d24bbd35bee0fde5354e291d4373
Gwithjoy <bangbabo131@gmail.com>
2025-04-29 12:33:13 +0900

    feat: 게시물 태그 기능 구현

--- src/main/java/com/team1/mixIt/tag/entity/TagSearchLog.java
+++ src/main/java/com/team1/mixIt/tag/entity/TagSearchLog.java
@@ -1,4 +1,33 @@
 package com.team1.mixIt.tag.entity;
 
+import com.team1.mixIt.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tag_search_log",
+        indexes = {@Index(name = "idx_tag_searched_at",
+                          columnList = "tag, searched_at")})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class TagSearchLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String tag;
+
+    @Column(name = "searched_at", nullable = false)
+    private LocalDateTime searchedAt = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
 }
---
+++ src/main/java/com/team1/mixIt/tag/repository/TagSearchLogRepository.java
@@ -0,0 +1,7 @@
+package com.team1.mixIt.tag.repository;
+
+import com.team1.mixIt.tag.entity.TagSearchLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagSearchLogRepository  extends JpaRepository<TagSearchLog, Long> {
+}
