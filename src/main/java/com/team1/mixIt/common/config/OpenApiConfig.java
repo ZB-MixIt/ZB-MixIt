13dbac7d11c43b7cd617bb649970bba44fbc1b83
Gwithjoy <bangbabo131@gmail.com>
2025-05-07 12:25:21 +0900

    refactor: openapiconfig 추가

---
+++ src/main/java/com/team1/mixIt/common/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+// src/main/java/com/team1/mixIt/common/config/OpenApiConfig.java
+package com.team1.mixIt.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.*;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "MixIt API", version = "v1"),
+        security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@Configuration
+public class OpenApiConfig { }
