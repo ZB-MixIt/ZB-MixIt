498f034a9a79f2adaa6fa06e880fd9409dd43cf2
Gwithjoy <bangbabo131@gmail.com>
2025-05-07 23:09:03 +0900

    fix: 북마크 목록 조회 개선

--- src/main/java/com/team1/mixIt/post/repository/UserBookmarkRepository.java
+++ src/main/java/com/team1/mixIt/post/repository/UserBookmarkRepository.java
@@ -2,16 +2,25 @@ package com.team1.mixIt.post.repository;
 
 import com.team1.mixIt.post.entity.UserBookmark;
 import com.team1.mixIt.post.entity.UserBookmarkId;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserBookmarkRepository extends JpaRepository<UserBookmark, UserBookmarkId> {
-
-    @EntityGraph(attributePaths =  {"post", "post.user"})
-    Page<UserBookmark> findAllByIdUserId(Long userId, Pageable pageable);
-
+    @Query("""
+        SELECT ub 
+        FROM UserBookmark ub
+        JOIN FETCH ub.post p
+        JOIN FETCH p.user               
+        WHERE ub.id.userId = :userId
+    """)
+    Page<UserBookmark> findAllByIdUserId(
+            @Param("userId") Long userId,
+            Pageable pageable
+    );
 }
