203f456fe600930ed6b30bb80b9291c232920301
Gwithjoy <bangbabo131@gmail.com>
2025-05-30 14:46:58 +0900

    fix: 리뷰 요청 DTO imageIds 유효성 검사 수정

--- src/main/java/com/team1/mixIt/post/dto/request/ReviewRequest.java
+++ src/main/java/com/team1/mixIt/post/dto/request/ReviewRequest.java
@@ -2,10 +2,10 @@ package com.team1.mixIt.post.dto.request;
 
 import com.team1.mixIt.common.validation.NoBannedWords;
 import com.team1.mixIt.common.validation.NoPersonalInfo;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
@@ -20,5 +20,6 @@ public class ReviewRequest {
     @NoPersonalInfo(message = "개인정보가 포함되어 있어요")
     private String content;
 
-    private List<@Size(max = 10, message = "이미지 ID는 최대 10개까지 등록될 수 있습니다.") Long> imageIds;
+    @Size(max = 10, message = "이미지는 최대 10개까지 등록할 수 있습니다.")
+    private List<Long> imageIds;
 }
