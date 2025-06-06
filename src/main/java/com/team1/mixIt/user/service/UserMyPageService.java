b021a9c6f27b0169fb663df6846569ac896cf871
lofri <lofri97@gmail.com>
2025-06-06 11:21:24 +0900

    feat: 마이페이지 수정

--- src/main/java/com/team1/mixIt/user/service/UserMyPageService.java
+++ src/main/java/com/team1/mixIt/user/service/UserMyPageService.java
@@ -1,23 +1,32 @@
 package com.team1.mixIt.user.service;
 
+import com.team1.mixIt.common.code.ResponseCode;
+import com.team1.mixIt.common.exception.ClientException;
 import com.team1.mixIt.image.entity.Image;
+import com.team1.mixIt.image.repository.ImageRepository;
 import com.team1.mixIt.image.service.ImageService;
 import com.team1.mixIt.post.entity.Review;
 import com.team1.mixIt.post.repository.ReviewRepository;
 import com.team1.mixIt.user.controller.UserMyPageController;
+import com.team1.mixIt.user.entity.User;
+import com.team1.mixIt.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class UserMyPageService {
 
     private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
     private final ImageService imageService;
+    private final ImageRepository imageRepository;
 
 
     public Page<UserMyPageController.MyReviewDto> getMyReviews(Long userId, Pageable pageable) {
@@ -31,4 +40,26 @@ public class UserMyPageService {
         });
     }
 
+    @Transactional
+    public User updateMyPage(User user, UserMyPageController.UpdateMyPageRequest dto) {
+        user = userRepository.findById(user.getId()).orElseThrow(() -> new ClientException(ResponseCode.USER_NOT_FOUND));
+
+        if (!user.getNickname().equals(dto.nickname())) {
+            userRepository.findByNickname(dto.nickname()).ifPresent(v -> {
+                throw new ClientException(ResponseCode.DUPLICATE_NICKNAME);
+            });
+        }
+
+        if (Objects.nonNull(dto.imageId())) {
+            Image image = imageService.findById(user.getId());
+
+            if (Objects.nonNull(image.getUser())) throw new ClientException(ResponseCode.IMAGE_OWNER_ALREADY_EXIST);
+            image.updateUser(user);
+            user.updateProfileImage(image);
+            imageRepository.save(image);
+        }
+        user.updateNotifyOn(dto.notification());
+        user.updatePushOn(dto.alarm());
+        return user;
+    }
 }
