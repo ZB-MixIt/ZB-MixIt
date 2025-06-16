f4029d35cc3bd2459e896f315b11b9fa1f7b98e5
Gwithjoy <bangbabo131@gmail.com>
2025-06-16 13:50:22 +0900

    fix: PostResponse defaultImage를 첫 번째 첨부 이미지로 설정하도록 수정

--- src/main/java/com/team1/mixIt/post/dto/response/PostResponse.java
+++ src/main/java/com/team1/mixIt/post/dto/response/PostResponse.java
@@ -111,8 +111,8 @@ public class PostResponse {
                 .toList();
 
         // 대표이미지
-        String def = defaultImageUrl;
-
+        String def = imgDtos.isEmpty() ? defaultImageUrl : imgDtos.get(0).getSrc();
+        
         // 작성자 여부, 북마크 여부
         boolean authorFlag = currentUserId != null && p.getUserId().equals(currentUserId);
 
