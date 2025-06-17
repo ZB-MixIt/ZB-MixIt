1696db9a7124462376aa25c9db9f1807792fb46b
Gwithjoy <bangbabo131@gmail.com>
2025-06-17 21:41:09 +0900

    feat: 인기 게시물 컨트롤러 로직 수정

--- src/main/java/com/team1/mixIt/post/dto/response/PostResponse.java
+++ src/main/java/com/team1/mixIt/post/dto/response/PostResponse.java
@@ -111,8 +111,8 @@ public class PostResponse {
                 .toList();
 
         // 대표이미지
-        String def = imgDtos.isEmpty() ? defaultImageUrl : imgDtos.get(0).getSrc();
-        
+        String def = defaultImageUrl;
+
         // 작성자 여부, 북마크 여부
         boolean authorFlag = currentUserId != null && p.getUserId().equals(currentUserId);
 
